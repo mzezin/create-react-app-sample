cc6e812052c73f85c65cf5c32031baaf5b576a45
Maksim Zezin <mzezin@gmail.com>
2019-01-31 16:20:51 +0300

    Airbnb fixes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        BOO
+      </div>
+    );
+  }
+}
+
+export default App;
