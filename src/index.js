136bc6c86d76a1007e2adff3cf246a4e4ebd8d55
mzezin <mzezin@gmail.com>
2019-07-28 00:40:02 +0300

    update and redux sample added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-underscore-dangle */
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
-serviceWorker.register();
+import 'antd/dist/antd.css';
+
+import initStore from './store';
+import App from './components/App';
+
+render(React.createElement(Provider, { store: initStore() }, React.createElement(App)), document.getElementById('root'));
